5a25d9a84958bba3253cc869b93ad6e24a4fce18
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-02-09 21:18:58 +0500

    beta version

--- js/popup.js
+++ js/popup.js
@@ -1,39 +1,68 @@
 const overlay = document.querySelector('.popup')
-const popUpOpenButton = document.querySelector('.profile__button-edit')
-const popUpCloseButton = document.querySelector('.popup__button-close')
-const name = document.querySelector('.profile__name')
-const profession = document.querySelector('.profile__profession')
-const formElement = document.querySelector('.popup__input')// Воспользуйтесь методом querySelector()
-const nameInput = document.querySelector('.popup__field_name') // Воспользуйтесь инструментом .querySelector()
-const jobInput = document.querySelector('.popup__field_profession')// Воспользуйтесь инструментом .querySelector()
-function togglePopup(evt) {
-  evt.preventDefault()
-  overlay.classList.toggle('popup__opened')
-
-}
-
-function closePopup(evt) {
-  if (evt.target === evt.currentTarget) {
-    togglePopup(evt)
+const profilePopup = document.querySelector('.profilepopup') // Попап для редактирования информации профиля
+const photoPopup = document.querySelector('.photopopup') //  Попап для редактирования фотокарточек
+const popUpEditButton = document.querySelector('.profile__button-edit') // Клавиша редактирования профиля
+const popUpAddButton = document.querySelector('.profile__button-add') // Клавиша добавления фотографии
+const profileCloseButton = profilePopup.querySelector('.popup__button-close') // Клавиша закрытия попапа в профиле
+const photoCloseButton = photoPopup.querySelector('.popup__button-close') // Клавиша закрытия попапа в фото
+const name = document.querySelector('.profile__name') // Ищем имя профиля на странице
+const profession = document.querySelector('.profile__profession') // Ищем профессию профился на странице
+const formElement = profilePopup.querySelector('.popup__input')// Форма для ввода информации о профиле
+const nameInput = document.querySelector('.popup__field_name') // Строка для ввода имени профиля
+const jobInput = document.querySelector('.popup__field_profession')// Строка для ввода професии профиля
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
+];
+const openPopup = (popupEl) => {
+  popupEl.classList.add('popup__opened');
+} // Функция открытия попапа
+popUpAddButton.addEventListener('click', () => {
+  openPopup(photoPopup);
+}) // Открытие попапа по нажатию на клавишу добавить
+popUpEditButton.addEventListener('click', () => {
+  openPopup(profilePopup);
+  nameInput.value = name.textContent;
+  jobInput.value = profession.textContent;
+}) // Открытие попапа по нажатию на клавишу редактировать
 
-}
-function openPopup() {
-  overlay.classList.add('popup__opened')
-  nameInput.value = (name.textContent)
-  jobInput.value = (profession.textContent)
-}
+const closePopup = (popupEl) => {
+  popupEl.classList.remove('popup__opened')
+} // Функция закрытия попапа
+profileCloseButton.addEventListener('click', () => {
+  closePopup(profilePopup)
+})
+photoCloseButton.addEventListener('click', () => {
+  closePopup(photoPopup)
+})
 
-
-function formSubmitHandler(evt) {
+function submitProfileForm(evt) {
   evt.preventDefault()
   name.textContent = (nameInput.value)
   profession.textContent = (jobInput.value)
-  closePopup(evt)
-}
+  closePopup(profilePopup)
+} // Передаем значения из формы на страницу
 
-formElement.addEventListener('submit', formSubmitHandler);
-popUpOpenButton.addEventListener('click', openPopup)
-popUpCloseButton.addEventListener('click', closePopup)
-overlay.addEventListener('click', closePopup)
-
+formElement.addEventListener('submit', submitProfileForm);
