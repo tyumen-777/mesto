ede801123cdac8f1824ec0822355ffe8b8bd7b7b
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-04-12 22:07:58 +0500

    Бета версия

---
+++ src/js/Api.js
@@ -0,0 +1,75 @@
+export default class Api {
+  constructor({address, token}) {
+    this._address = address
+    this._token = token
+  }
+
+  getInitialCards() {
+    return fetch(`${this._address}/cards`, {
+      headers: {
+        authorization: this._token
+      }
+    }).then(response => response.ok
+      ? response.json()
+      : Promise.reject(`Ошибка ${response.status}`))
+  } // Получаем массив карточек с сервера
+
+  getUserInfo() {
+    return fetch(`${this._address}/users/me`, {
+      headers: {
+        authorization: this._token
+      }
+    }).then(response => response.ok
+      ? response.json()
+      : Promise.reject(`Ошибка ${response.status}`))
+  } // Получаем информацию о пользователе с сервера
+
+  editUserInfo(name, profession) {
+    return fetch(`${this._address}/users/me`, {
+      method: 'PATCH',
+      headers: {
+        authorization: this._token,
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        about: profession
+      })
+    }).then(response => response.ok
+      ? response.json()
+      : Promise.reject(`Ошибка ${response.status}`))
+  }
+
+  addCard(name, link) {
+    return fetch(`${this._address}/cards`, {
+      method: 'POST',
+      headers: {
+        authorization: this._token,
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        link: link
+      })
+    }).then(response => response.ok
+      ? response.json()
+      : Promise.reject(`Ошибка ${response.status}`))
+  }
+
+  editUserAvatar(url) {
+    console.log(url)
+    return fetch(`${this._address}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: {
+        authorization: this._token,
+        'Content-type': 'application/json'
+
+      },
+      body: JSON.stringify({
+        avatar: url
+      })
+    }).then(response => response.ok
+      ? response.json()
+      : Promise.reject(`Ошибка ${response.status}`))
+  }
+}
